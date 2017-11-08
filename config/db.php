5b0cf7967decf47bfd3ad5d845f55efdaef359bf
Мар’ян <mbabych@yedynka.com>
2017-11-08 12:03:13 +0200

    set params

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=impuls',
+    'dsn' => 'mysql:host=localhost;dbname=name',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
